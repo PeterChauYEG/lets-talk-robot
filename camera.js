f2d2fa4dfe641e0d9db62968086bc58e14685919
Peter C <peter.chau.esq@gmail.com>
2016-12-15 04:11:24 +0000

    hook up gpio to the pi. run status = untested

--- camera.js
+++ camera.js
@@ -5,24 +5,24 @@ var proc;
 
 
 function stopStreaming() {
-    if (proc) {
-        proc.kill();
-        fs.unwatchFile('./stream/image_stream.jpg');
-    }
+  if (proc) {
+    proc.kill();
+    fs.unwatchFile('./stream/image_stream.jpg');
+  }
 }
 
 function startStreaming(io) {
-    var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-    proc = spawn('raspistill', args);
+  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+  proc = spawn('raspistill', args);
 
-    console.log('Watching for changes...');
+  console.log('Watching for changes...');
 
-    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-    });
+  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+  });
 }
 
 export default {
-    stopStreaming,
-    startStreaming
+  stopStreaming,
+  startStreaming
 }
--- index.js
+++ index.js
@@ -15,113 +15,161 @@ const drivetrain = gpio.drivetrain;
 
 // Create board with gpio
 const board = new five.Board({
-    io: new raspi()
+  io: new raspi()
 });
 
 var sockets = {};
 
 app.use('/', express.static(path.join(__dirname, 'stream')));
 
+// serve client
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
+// client connection
 io.on('connection', function(socket) {
-    sockets[socket.id] = socket;
-    console.log("Total clients connected : " + Object.keys(sockets).length);
+  sockets[socket.id] = socket;
+  console.log("Total clients connected : " + Object.keys(sockets).length);
 
-    io.emit('log message', 'a user has connected');
-    console.log('a user connected');
+  io.emit('log message', 'a user has connected');
+  console.log('a user connected');
 
-    socket.on('disconnect', function() {
-        delete sockets[socket.id];
+  // client disconnection
+  socket.on('disconnect', function() {
+    delete sockets[socket.id];
 
-        io.emit('log message', 'a user has disconnected');
-        console.log('user disconnected');
+    // if no more sockets, kill the stream
+    if (Object.keys(sockets).length == 0) {
+      app.set('watchingFile', false);
+      camera.stopStreaming();
+    }
 
-        // if no more sockets, kill the stream
-        if (Object.keys(sockets).length == 0) {
-            app.set('watchingFile', false);
-            camera.stopStreaming();
-        }
-    });
+    io.emit('log message', 'a user has disconnected');
+    console.log('user disconnected');
+  });
 
-    socket.on('start-stream', function() {
-        io.emit('log message', 'starting video streat');
+  // to start a stream
+  socket.on('start-stream', function() {
+    io.emit('log message', 'starting video stream');
 
-        if (app.get('watchingFile')) {
-            io.sockets.emit('liveStream', 'image_stream.jpg?=' + (Math.random() * 100000));
-        }
-        else {
-            camera.startStreaming(io);
-            app.set('watchingFile', true);
-        }
+    if (app.get('watchingFile')) {
+      io.sockets.emit('liveStream', 'image_stream.jpg?=' + (Math.random() * 100000));
+    }
+    else {
+      camera.startStreaming(io);
+      app.set('watchingFile', true);
+    }
 
-    });
+  });
 
-    socket.on('log message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('log message', msg);
-    })
+  // log message to client
+  socket.on('log message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('log message', msg);
+  })
 
-    socket.on('gpio', function(req) {
-        if (req == 'initialize') {
-            // Initialize board
-            board.on('ready', function() {
+  // drive gpio
+  socket.on('gpio', function(req) {
 
-                // initialize motors
-                gpio.setdrivetrain(drivetrain, 1, 1);
-                gpio.setdrivetrain(drivetrain, 0, 0);
+    if (req == 'initialize') {
+      // Initialize board
+      board.on('ready', function() {
+        io.emit('log message', 'board ready');
 
-                // Set Software state LED to "board-ready"
-                gpio.setLED(LED, 'board-ready');
+        // initialize motors
+        gpio.setdrivetrain(drivetrain, 1, 1);
+        gpio.setdrivetrain(drivetrain, 0, 0);
 
-                // Set Software state LED to "error connecting-to-server"
-                gpio.setLED(LED, 'error-connecting-to-server');
+        // Set Software state LED to "board-ready"
+        gpio.setLED(LED, 'board-ready');
 
-                // Set Software state LED to "connected-to-server"
-                gpio.setLED(LED, 'connected-to-server');
+        // Set Software state LED to "error connecting-to-server"
+        gpio.setLED(LED, 'error-connecting-to-server');
 
-                // gpiO.setdrivetrain(drivetrain, AIN, BIN);
-                gpio.setLED(LED, 'board-response');
+        // Set Software state LED to "connected-to-server"
+        gpio.setLED(LED, 'connected-to-server');
 
-                gpio.setLED(LED, 'reconnected-to-server');
-                gpio.setLED(LED, 'server-pipe');
+        // gpiO.setdrivetrain(drivetrain, AIN, BIN);
+        gpio.setLED(LED, 'board-response');
 
-                // Handle board shutdown
-                board.on('warn', function(event) {
-                    console.log(event.message + '...');
-                    if (event.message === 'Closing.') {
+        gpio.setLED(LED, 'reconnected-to-server');
+        gpio.setLED(LED, 'server-pipe');
 
-                        // Turn off motors
-                        console.log('shutting down board...');
-                        gpio.setdrivetrain(drivetrain, 0, 0);
+        // handle gpio
+        socket.on('gpio', function(req) {
+          switch (req) {
+            case 'forward':
+              console.log('message: ' + req);
+              io.emit('log message', req);
 
-                        // Set Software state LED to "board-off"
-                        console.log('talk to you later bae <3');
-                        gpio.setLED(LED, 'board-off');
-                    }
-                });
-            });
-        }
+              // motors forward
+              gpio.setdrivetrain(drivetrain, 1, 1);
+              break
+            case 'rotate right':
+              console.log('message: ' + req);
+              io.emit('log message', req);
 
-        if (req == 'shutdown') {
-            // if no more sockets, kill the stream
-            if (Object.keys(sockets).length == 0) {
+              // motors rotate right
+              gpio.setdrivetrain(drivetrain, 1, 0);
+              break
+            case 'reverse':
+              console.log('message: ' + req);
+              io.emit('log message', req);
 
-                // Turn off motors
-                console.log('shutting down board...');
-                gpio.setdrivetrain(drivetrain, 0, 0);
+              // motors reverse
+              gpio.setdrivetrain(drivetrain, 1, 1);
+              break
+            case 'rotate left':
+              console.log('message: ' + req);
+              io.emit('log message', req);
 
-                // Set Software state LED to "board-off"
-                console.log('talk to you later bae <3');
-                gpio.setLED(LED, 'board-off');
-            }
-        }
+              // motors rotate left
+              gpio.setdrivetrain(drivetrain, 0, 1);
+              break
+            case 'stop':
+            default:
+              console.log('message: ' + req);
+              io.emit('log message', req);
 
-    })
+              // stop motors
+              gpio.setdrivetrain(drivetrain, 0, 0);
+          }
+        })
+
+        // Handle board shutdown
+        board.on('warn', function(event) {
+          console.log(event.message + '...');
+          if (event.message === 'Closing.') {
+
+            // Turn off motors
+            console.log('shutting down board...');
+            gpio.setdrivetrain(drivetrain, 0, 0);
+
+            // Set Software state LED to "board-off"
+            console.log('talk to you later bae <3');
+            gpio.setLED(LED, 'board-off');
+          }
+        });
+      });
+    }
+
+    if (req == 'shutdown') {
+      // if no more sockets, kill the stream
+      if (Object.keys(sockets).length == 0) {
+
+        // Turn off motors
+        console.log('shutting down board...');
+        gpio.setdrivetrain(drivetrain, 0, 0);
+
+        // Set Software state LED to "board-off"
+        console.log('talk to you later bae <3');
+        gpio.setLED(LED, 'board-off');
+      }
+    }
+  })
 })
 
 http.listen(8080, function() {
-    console.log('listening on *:8080');
+  console.log('listening on *:8080');
 });
