38386346e3fe075980f755d114eb26cf03acd780
Peter C <peter.chau.esq@gmail.com>
2016-12-17 22:31:27 +0000

    fixing camera exports again

--- camera.js
+++ camera.js
@@ -4,14 +4,14 @@ var spawn = require('child_process').spawn;
 var proc;
 
 
-function stopStreaming() {
+export function stopStreaming() {
   if (proc) {
     proc.kill();
     fs.unwatchFile('./stream/image_stream.jpg');
   }
 }
 
-function startStreaming(io) {
+export function startStreaming(io) {
   var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
   proc = spawn('raspistill', args);
 
@@ -20,9 +20,4 @@ function startStreaming(io) {
   fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
     io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
   });
-}
-
-export default {
-  stopStreaming,
-  startStreaming
 }
