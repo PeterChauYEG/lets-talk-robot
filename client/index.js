55fd6f68b40d6f34a764d4b22e488005ae104148
Peter C <peter.chau.esq@gmail.com>
2017-01-29 03:33:44 +0000

    route the servers a bit on the cilent side

--- client/index.js
+++ client/index.js
@@ -27,7 +27,7 @@ var canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
 
 // Create h264 player
-var uri = "ws://" + document.location.host;
+var uri = "ws://192.168.0.23:8081";
 var wsavc = new WSAvcPlayer(canvas, "webgl", 1, 35);
 wsavc.connect(uri);
 
