1d2d380e56fe3c89cc4f49f7ef6ccb8c8e3775bf
PeterChauYEG <peter.chau.esq@gmail.com>
2018-03-24 16:03:30 -0600

    recieving gpio and handling it

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import robot deps
-// import five from 'johnny-five';
-// import raspi from 'raspi-io';
+import five from 'johnny-five';
+import raspi from 'raspi-io';
 
 // import deps
 import io from 'socket.io-client'
 
 // import hardware interfaces
-// import { setDrivetrain } from './drivers/drv8833';
+import { setDrivetrain } from './drivers/drv8833';
 
 // setup hardware api
 var sockets = {};
@@ -14,74 +14,71 @@ var sockets = {};
 // setup socket
 const socket = io('http://localhost:8080')
 
-// // Create board with gpio
-// const board = new five.Board({
-//   io: new raspi()
-// });
+// Create board with gpio
+const board = new five.Board({
+  io: new raspi()
+});
 
 // Initialize board
-// board.on('ready', function() {
-//
-//   // POLOLU DRV8833 Dual H-bridge Configuration
-//   const drivetrain = {
-//
-//     // right motor
-//   	ain: new five.Motor({
-//   		pins: {
-//   			pwm: 24, // white wire // AIN2
-//   			dir: 2 // red wire // AIN1
-//   		},
-//   		invertPWM: true
-//   	}),
-//
-//   	// left motor
-//   	bin: new five.Motor({
-//   		pins: {
-//   			pwm: 26, // brown wire // BIN2
-//   			dir: 7 // black wire // BIN1
-//   		},
-//   		invertPWM: true
-//   	})
-//   };
-//
-//   // initialize motors
-//   setDrivetrain(drivetrain, 1, 1);
-//   setDrivetrain(drivetrain, 0, 0);
-//
+board.on('ready', function() {
+
+  // POLOLU DRV8833 Dual H-bridge Configuration
+  const drivetrain = {
+
+    // right motor
+  	ain: new five.Motor({
+  		pins: {
+  			pwm: 24, // white wire // AIN2
+  			dir: 2 // red wire // AIN1
+  		},
+  		invertPWM: true
+  	}),
+
+  	// left motor
+  	bin: new five.Motor({
+  		pins: {
+  			pwm: 26, // brown wire // BIN2
+  			dir: 7 // black wire // BIN1
+  		},
+  		invertPWM: true
+  	})
+  };
+
+  // initialize motors
+  setDrivetrain(drivetrain, 1, 1);
+  setDrivetrain(drivetrain, 0, 0);
+
   socket.emit('log message', 'board ready');
+  console.log('ready')
 
-  // client connection
-  socket.on('connection', function(gpio) {
+  // handle gpio
+  socket.on('gpio', function(msg) {
+    switch (msg) {
 
-    // handle gpio
-    gpio.on('gpio', function(req) {
-      switch (req) {
+      case 'forward':
+        setDrivetrain(drivetrain, 1, 1);
+        console.log('gpio: ' + msg);
+        break;
 
-        case 'forward':
-          // setDrivetrain(drivetrain, 1, 1);
-          console.log('message: ' + req);
-          break;
+      case 'rotate right':
+        setDrivetrain(drivetrain, -1, 1);
+        console.log('gpio: ' + msg);
+        break;
 
-        case 'rotate right':
-          // setDrivetrain(drivetrain, -1, 1);
-          console.log('message: ' + req);
-          break;
+      case 'backwards':
+        setDrivetrain(drivetrain, -1, -1);
+        console.log('gpio: ' + msg);
+        break;
 
-        case 'backwards':
-          // setDrivetrain(drivetrain, -1, -1);
-          console.log('message: ' + req);
-          break;
+      case 'rotate left':
+        setDrivetrain(drivetrain, 1, -1);
+        console.log('gpio: ' + msg);
+        break;
 
-        case 'rotate left':
-          // setDrivetrain(drivetrain, 1, -1);
-          console.log('message: ' + req);
-          break;
-
-        case 'stop':
-        default:
-          // setDrivetrain(drivetrain, 0, 0);
-          console.log('message: ' + req);
-      }
-    });
+      case 'stop':
+      default:
+        setDrivetrain(drivetrain, 0, 0);
+        console.log('gpio: ' + msg);
+    }
   });
-// });
+});
